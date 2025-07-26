eca98a4a11c59650059671f348a23a418a366fc0
TegaJeremy <oghenedemartin@gmail.com>
2025-07-26 17:02:48 +0100

    updting file  for group

--- config/database.py
+++ config/database.py
@@ -1,34 +1,16 @@
 from pymongo import MongoClient
 
 
-uri = "mongodb+srv://oghenedemartin:cvQCKBHtcjLmSuoH@medicalsystem.xb62ss8.mongodb.net/?retryWrites=true&w=majority&appName=medicalSystem"
+def get_database():
+    uri = "mongodb+srv://oghenedemartin:cvQCKBHtcjLmSuoH@medicalsystem.xb62ss8.mongodb.net/?retryWrites=true&w=majority&appName=medicalSystem"
 
+    client = MongoClient(uri)
+    db = client["medical_db"]
 
-client = MongoClient(uri)
-
-
-db = client["medical_db"]
-doctors_collection = db["doctors"]
-
-
-doctor = {
-    "name": "Dr. John Doe",
-    "password": "securepass",
-    "specialisation": "Cardiologist",
-    "contact": {
-        "name": "Dr. John",
-        "phone_no": "08012345678",
-        "email": "john@example.com",
-        "address": {
-            "house_no": "12",
-            "street": "Health Ave",
-            "city": "Lagos",
-            "state": "Lagos"
-        }
+    collections = {
+        "doctors": db["doctors"],
+        "patients": db["patients"],
+        "records": db["records"]
     }
-}
 
-
-result = doctors_collection.insert_one(doctor)
-
-print("Doctor inserted with _id:", result.inserted_id)
+    return collections
