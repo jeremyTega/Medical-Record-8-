7567e9d2dbc716da70d044700cd0945733600416
Hilary <hilarykaior@gmail.com>
2025-07-26 14:34:00 +0100

    diagnosis entry and medical history with tests, patient commits that may need to be refactored

---
+++ medical_application/dummy_appointment.py
@@ -0,0 +1,6 @@
+class Appointment:
+    def __init__(self, apt_id, apt_date, apt_time, reason):
+        self.apt_id = apt_id
+        self.apt_date = apt_date
+        self.apt_time = apt_time
+        self.reason = reason
